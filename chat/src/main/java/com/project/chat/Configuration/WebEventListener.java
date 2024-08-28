0ed6eb2451d829eac3e89d1ca9d4cc3f995046e1
gabeman <gabrielayochukwu@gmail.com>
2024-08-28 12:02:57 +0100

    configured websocketconfig and create event disconnect listener

---
+++ chat/src/main/java/com/project/chat/Configuration/WebEventListener.java
@@ -0,0 +1,20 @@
+package com.project.chat.Configuration;
+
+import jakarta.websocket.Session;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebEventListener {
+    @EventListener
+    public void handleWebSocketDisconnectListener(
+            SessionDisconnectEvent event
+    ){
+
+    }
+}
